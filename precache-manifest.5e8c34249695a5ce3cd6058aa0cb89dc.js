b084dd24f45133b15674ad194490eb0b97cd3765
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-02-27 02:50:33 +0600

    Updates

---
+++ precache-manifest.5e8c34249695a5ce3cd6058aa0cb89dc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/react_weatherapp/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react_weatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react_weatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react_weatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react_weatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "d73fc65b9acb7253a38c",
+    "url": "/react_weatherapp/static/js/runtime~main.d73fc65b.js"
+  },
+  {
+    "revision": "939a7712c2989443b53e",
+    "url": "/react_weatherapp/static/js/main.939a7712.chunk.js"
+  },
+  {
+    "revision": "ec0c52003545f766256d",
+    "url": "/react_weatherapp/static/js/2.ec0c5200.chunk.js"
+  },
+  {
+    "revision": "939a7712c2989443b53e",
+    "url": "/react_weatherapp/static/css/main.2fe45d27.chunk.css"
+  },
+  {
+    "revision": "dd3042fa43d4e800370db6acbbf8142f",
+    "url": "/react_weatherapp/index.html"
+  }
+];
